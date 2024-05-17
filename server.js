aa18b1e1fadf7482c42b77faf12548441bc068f7
BalramMardi <120470787+BalramMardi@users.noreply.github.com>
2024-05-17 20:14:32 +0530

    updated server

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import AuthRoutes from "./routes/AuthRoutes.js";
 import Watchlist from "./routes/Watchlist.js";
-
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 //config
 dotenv.config();
@@ -14,24 +15,27 @@ dotenv.config();
 //database
 connectDB();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", AuthRoutes);
 app.use("/api/v1/stock", Watchlist);
 
-app.get("/", (req, res) => {
-  res.send({
-    message: "Welcome to the app",
-  });
+//rest api
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`.bgCyan.white);
